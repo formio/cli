ed56b98cdb12828f3b67a701c23d3c109742d366
Travis Tidwell <codertravist@gmail.com>
2015-09-30 08:47:18 -0500

    Updating the intro screen.

--- index.js
+++ index.js
@@ -2,8 +2,8 @@ require('colors');
 var fs = require('fs-extra');
 var program = require('commander');
 
-// Print the welcome screen.
-console.log(fs.readFileSync(__dirname + '/welcome.txt').toString().green);
+// Show welcome.
+require(__dirname + '/src/welcome/welcome');
 
 // Register all the commands.
 require(__dirname + '/commands/commands')(program, function(err) {
---
+++ src/welcome/welcome.js
@@ -0,0 +1,22 @@
+var fs = require('fs-extra');
+require('colors');
+console.log('');
+var rl = require('readline').createInterface({
+    input: fs.createReadStream(__dirname + '/logo.txt')
+});
+
+rl.on('line', function (line) {
+    console.log(
+        line.substring(0,4) +
+        line.substring(4, 30).cyan.bold +
+        line.substring(30, 33) +
+        line.substring(33, 42).green.bold +
+        line.substring(42)
+    );
+});
+
+rl.on('close', function() {
+    // Print the welcome screen.
+    console.log('');
+    console.log(fs.readFileSync(__dirname + '/welcome.txt').toString().green);
+});
