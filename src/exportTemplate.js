a92a34d2c3cfa4300fd68cd2b86358978c6cafa2
Randall Knutson <randall@form.io>
2015-11-09 15:45:15 -0600

    Limit deploy command to only projects on paid plans.

--- src/exportTemplate.js
+++ src/exportTemplate.js
@@ -5,6 +5,9 @@ module.exports = function(options, next) {
   var project = new formio.Project(options.project);
   project.export().then(function() {
     options.template = project.template;
+    if (project.template.plan && project.template.plan === 'community') {
+      return next('Deploy is only available for projects on a paid plan.');
+    }
     next();
   }).catch(next);
 };
