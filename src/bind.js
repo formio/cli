7424211b8cda12d2d0428f82f2554ec80be60862
Travis Tidwell <travis@form.io>
2015-11-23 21:59:58 -0600

    Fixed an issue with the domain for the server.

--- src/bind.js
+++ src/bind.js
@@ -1,9 +1,17 @@
 module.exports = function(options, next) {
-    var formio = require('../src/formio')(options);
     var method = options.params[0];
     var url = options.params[1];
     var parts = url.split('://');
     var subparts = parts[1].split('/');
+    var hostparts = subparts[0].split('.');
+    if (
+        (hostparts.length > 2) ||
+        ((hostparts.length > 1) && hostparts[1].indexOf('localhost') !== -1)
+    ) {
+        hostparts.shift();
+    }
+    options.server = parts[0] + '://' + hostparts.join('.');
+    var formio = require('../src/formio')(options);
     var project = new formio.Project(parts[0] + '://' + subparts[0]);
     project.load().then(function() {
         project.bind(subparts[1], method, function(err, data) {
