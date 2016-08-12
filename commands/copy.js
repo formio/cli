5c73acdfeec2024996ea745c4e4c875492f44f0b
Randall Knutson <randall@form.io>
2016-08-12 12:18:08 -0500

    Add options to copy command and update formio-servce which contains a fix for authentication.

--- commands/copy.js
+++ commands/copy.js
@@ -8,6 +8,8 @@ module.exports = function(program, next) {
     .description('Copy a form or project from one source to a destination.')
     .option('-p, --protocol [protocol]', 'Change the protocol.')
     .option('-h, --host [host]', 'Set the host for the copy.')
+    .option('--username [username]', 'The source username to authenticate with')
+    .option('--password [password]', 'The source password')
     .action(series([
       require('../src/authenticate'),
       require('../src/copy')
