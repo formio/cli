a34696dd8b8797b754f59a167280092ec1607931
Travis Tidwell <travis@form.io>
2022-07-27 10:43:15 -0500

    Fixing clone tool to work with submission collections.

--- src/clone.js
+++ src/clone.js
@@ -2,7 +2,6 @@
 const mongodb = require('mongodb');
 const async = require('async');
 const _ = require('lodash');
-const fs = require('fs');
 const MongoClient = mongodb.MongoClient;
 
 module.exports = function (source, destination, options) {
@@ -35,11 +34,12 @@ module.exports = function (source, destination, options) {
 
     console.log(`Succesfully connected to ${source}`);
     const srcDb = _src.db(_src.s.options.dbName);
+    const srcSubs = srcDb.collection('submissions');
     const src = {
       db: srcDb,
       projects: srcDb.collection('projects'),
       forms: srcDb.collection('forms'),
-      submissions: srcDb.collection('submissions'),
+      submissions: srcSubs,
       roles: srcDb.collection('roles'),
       actions: srcDb.collection('actions'),
       formrevisions: srcDb.collection('formrevisions'),
@@ -56,11 +56,12 @@ module.exports = function (source, destination, options) {
       console.log(`Succesfully connected to ${destination}`);
       console.log('');
       const destDb = _dest.db(_dest.s.options.dbName);
+      const destSubs = destDb.collection('submissions');
       const dest = {
         db: destDb,
         projects: destDb.collection('projects'),
         forms: destDb.collection('forms'),
-        submissions: destDb.collection('submissions'),
+        submissions: destSubs,
         roles: destDb.collection('roles'),
         actions: destDb.collection('actions'),
         formrevisions: destDb.collection('formrevisions'),
@@ -183,6 +184,17 @@ module.exports = function (source, destination, options) {
         return newQuery;
       };
 
+      // Sets the submission collection for this form.
+      const setCollection = function (form, srcProj, destProj) {
+        const subsCollection = _.get(form, 'settings.collection');
+        if (!subsCollection) {
+          return;
+        }
+        console.log(`Using collection - ${subsCollection}`);
+        src.submissions = subsCollection ? src.db.collection(`${srcProj.name}_${subsCollection}`) : srcSubs;
+        dest.submissions = subsCollection ? dest.db.collection(`${destProj.name}_${subsCollection}`) : destSubs;
+      };
+
       if (options.submissionsOnly) {
         console.log(`Cloning all submissions from project ${options.srcProject} to ${options.dstProject}.`);
         if (!options.dstProject) {
@@ -193,45 +205,60 @@ module.exports = function (source, destination, options) {
           return console.log(`You must provide a source project. --src-project=<project_id>`);
         }
 
-        // Iterate through all forms.
-        eachDocument(dest.forms.find(getQuery({
-          project: mongodb.ObjectID(options.dstProject)
-        })), (dstForm, nextForm) => {
-          src.forms.findOne({
-            project: mongodb.ObjectID(options.srcProject),
-            name: dstForm.name
-          }, (err, srcForm) => {
+        // Load the source project.
+        src.projects.findOne({ _id: mongodb.ObjectID(options.srcProject) }, (err, srcProj) => {
+          if (err) {
+            return console.log(`Error loading source project: ${err.message}`);
+          }
+
+          // Load the destination project.
+          dest.projects.findOne({ _id: mongodb.ObjectID(options.dstProject) }, (err, destProj) => {
             if (err) {
-              return nextForm(err);
-            }
-            if (!srcForm) {
-              return nextForm();
+              return console.log(`Error loading destination project: ${err.message}`);
             }
 
-            console.log('');
-            let deletePromise = Promise.resolve();
-            if (options.deleteSubmissions) {
-              console.log(`Deleting submissions from ${dstForm.title}`);
-              deletePromise = dest.submissions.deleteMany(getItemQuery({
-                project: dstForm.project,
-                form: dstForm._id
-              }));
-            }
+            // Iterate through all forms.
+            eachDocument(dest.forms.find(getQuery({
+              project: mongodb.ObjectID(options.dstProject)
+            })), (dstForm, nextForm) => {
+              src.forms.findOne({
+                project: mongodb.ObjectID(options.srcProject),
+                name: dstForm.name
+              }, (err, srcForm) => {
+                if (err) {
+                  return nextForm(err);
+                }
+                if (!srcForm) {
+                  return nextForm();
+                }
 
-            deletePromise.then(() => {
-              process.stdout.write(`Cloning submissions from form ${srcForm.title}: `);
-              upsertAll('submissions', getItemQuery({
-                form: srcForm._id,
-                project: srcForm.project
-              }), (submission) => {
-                submission.form = dstForm._id;
-                submission.project = dstForm.project;
-              }, null, () => nextForm());
+                setCollection(srcForm, srcProj, destProj);
+                console.log('');
+                let deletePromise = Promise.resolve();
+                if (options.deleteSubmissions) {
+                  console.log(`Deleting submissions from ${dstForm.title}`);
+                  deletePromise = dest.submissions.deleteMany(getItemQuery({
+                    project: dstForm.project,
+                    form: dstForm._id
+                  }));
+                }
+
+                deletePromise.then(() => {
+                  process.stdout.write(`Cloning submissions from form ${srcForm.title}: `);
+                  upsertAll('submissions', getItemQuery({
+                    form: srcForm._id,
+                    project: srcForm.project
+                  }), (submission) => {
+                    submission.form = dstForm._id;
+                    submission.project = dstForm.project;
+                  }, null, () => nextForm());
+                });
+              });
+            }, () => {
+              console.log('');
+              console.log('DONE!');
             });
           });
-        }, () => {
-          console.log('');
-          console.log('DONE!');
         });
       }
       else {
@@ -273,6 +300,7 @@ module.exports = function (source, destination, options) {
             process.stdout.write("\n");
             process.stdout.write(`Cloning project ${project.title} (${project._id}): `);
             upsertAll('forms', itemQuery, (form) => {
+              setCollection(form, project, clonedProject);
               form.project = clonedProject._id;
             }, (form, clonedForm, nextForm) => {
               itemQuery.form = form._id;
