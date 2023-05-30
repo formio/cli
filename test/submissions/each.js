0602bc6826359ef938ceb28678435b54bb4793fb
Travis Tidwell <travis@form.io>
2023-05-30 15:18:14 -0500

    Refactoring clone command.

--- test/submissions/each.js
+++
@@ -1,6 +0,0 @@
-module.exports = function(record, index, options, next) {
-  if (record.data.firstName) {
-    console.log('SUBMISSION: ' + index + ': FirstName=' + record.data.firstName);
-  }
-  next();
-};
