68596a7c11614cd1b81a83fcbb2eb33412032f55
mikekotikov <kmikecoder@gmail.com>
2023-07-18 15:35:53 +0200

    FIO-6935: Update logo

--- src/welcome/welcome.js
+++ src/welcome/welcome.js
@@ -5,24 +5,8 @@ require('colors');
 
 module.exports = function(next) {
   console.log('');
-  var rl = require('readline').createInterface({
-    input: fs.createReadStream(__dirname + '/logo.txt')
-  });
-
-  rl.on('line', function(line) {
-    console.log(
-      line.substring(0,4) +
-      line.substring(4, 30).cyan.bold +
-      line.substring(30, 33) +
-      line.substring(33, 42).green.bold +
-      line.substring(42)
-    );
-  });
-
-  rl.on('close', function() {
-    // Print the welcome screen.
-    console.log('');
-    console.log(fs.readFileSync(__dirname + '/welcome.txt').toString().green);
-    next();
-  });
+  console.log(fs.readFileSync(__dirname + '/logo.txt').toString());
+  console.log('');
+  console.log(fs.readFileSync(__dirname + '/welcome.txt').toString().green);
+  next();
 };
